07bca89482100f3da9f8c7f1c10ad7cc2a00a088
Terence Chow <terence.chow.1986@gmail.com>
2014-03-11 01:49:42 +0000

    reading from file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+   fs.readFileSync('index.html',function(data){
+       response.send(data.toString('utf-8'));
+   });
+
 });
 
 var port = process.env.PORT || 5000;
