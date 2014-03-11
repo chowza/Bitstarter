5bf830aa87a7bab533f97baa5ca1c28b6cee86d4
Terence Chow <terence.chow.1986@gmail.com>
2014-03-11 02:09:10 +0000

    reading from file

--- web.js
+++ web.js
@@ -1,16 +1,13 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var buffer = new Buffer(20);
-app.use(express.logger());
+app.use(express.logger())
+
 
 app.get('/', function(request, response) {
-   fs.readFileSync('index.html',function(data){
-       buffer.write(data,'utf-8');
-       response.send(buffer.toString);
+   var myString = fs.readFileSync('index.html')
+         response.send(myString.toString);
    });
-
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
