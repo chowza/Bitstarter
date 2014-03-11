1e4dc53a3223e7ea803c07df22fa41dab7df0193
Terence Chow <terence.chow.1986@gmail.com>
2014-03-11 03:30:48 +0000

    update grader to be able to handle urls

--- grader.js
+++ grader.js
@@ -5,6 +5,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
 
 var assertFileExists = function(infile){
     var instr = infile.toString();
@@ -40,7 +41,20 @@ if (require.main ==module){
     program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists),CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>','Path to index.html',clone(assertFileExists),HTMLFILE_DEFAULT)
+    .option('u, --url <url>','Http URL')
     .parse(process.argv);
+    
+    if (program.url){
+    rest.get(program.url).on('complete',function(result){
+	if (result instanceof Error){
+	    console.log('Error:',result.message);
+	    this.retry(5000);
+	}
+	else {
+	    program.file = result;
+	    }
+    });
+    }
     var checkJson = checkHtmlFile(program.file,program.checks);
     var outJson = JSON.stringify(checkJson,null,4);
     console.log(outJson);
